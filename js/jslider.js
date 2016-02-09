5f2970e39692685f79c3fa2714b87bc3ce609a47
yijaejun <yijaejun81@gmail.com>
2016-02-09 18:58:10 +0900

    Reduce to do loop count and organize some variables

--- js/jslider.js
+++ js/jslider.js
@@ -10,7 +10,7 @@ function JSlider(settings){
   this.next = settings.next;
   this.imgPath = settings.imgPath;
   this.imgType = settings.imgType;
-  // current pointer on the preview (0,1,2)
+  
   this.currentPreview = settings.current; 
   
   this.btnPrev = settings.btnPrev;
@@ -24,6 +24,10 @@ function JSlider(settings){
   this.animate = settings.animate;
   this.animateDuration = settings.animateDuration;
   this.animateType = settings.animateType;
+  
+  // if setting's method is undefined or empty
+  
+  
 }
 
 JSlider.prototype = {
@@ -137,7 +141,7 @@ function showMainDisplay(slider, callback){
     @direction right or left
 */
 function displayPreview(slider, direction){
-    var str = "", i;
+    var i, set, el, elem;
     
     if(!slider.animate){
         if(slider.preview_setting.length <= 0 || slider.preview_setting === null){
@@ -160,9 +164,9 @@ function displayPreview(slider, direction){
                 var tmp2 = slider.preview_setting[1];
                 slider.preview_setting = 
                 [
-                tmp2-2 < 1 ? Math.abs(slider.total+tmp2-2) : tmp2-2,
-                tmp2-1 < 1 ? Math.abs(slider.total+tmp2-1) : tmp2-1,
-                tmp2 < 1 ? Math.abs(slider.total+1-tmp2) : tmp2
+                    tmp2-2 < 1 ? Math.abs(slider.total+tmp2-2) : tmp2-2,
+                    tmp2-1 < 1 ? Math.abs(slider.total+tmp2-1) : tmp2-1,
+                    tmp2 < 1 ? Math.abs(slider.total+1-tmp2) : tmp2
                 ];
             
             }else{
@@ -170,85 +174,39 @@ function displayPreview(slider, direction){
             }
         }
       
-        var set = slider.preview_setting;
+        set = slider.preview_setting;
+        slider.preview.children().remove();
         for(i=0;i<set.length;i++){ // 스트링 변수에 덮어 쓰는 방법보다 효율적인 방법은 무엇일까?
-            
-            str += '<a href="#none" class="img"><img src="' + slider.imgPath+set[i] + "." + 
+            elem = '<a href="#none" class="img"><img src="' + slider.imgPath+set[i] + "." + 
                 slider.imgType +'" alt="' + set[i] + '" width="57" height="60" /></a>';
-                
-            
-        }
-
-        slider.preview.addClass('invisible');
-        slider.preview.html(str);
-
-        // 여기서 위치를 조정한다. 결국 여기서 또 루프를 돌게 된다. 이를 피할 수 있는 방법이 있을까?
-        for(var el=0;el<slider.preview_setting.length;el++){
-            $('.img').eq(el).css({
-                'left' : 73*el + 'px'
+            slider.preview.append(elem).find('.img').eq(i).css({
+                'left' : 72*i + 'px'
             });
-        }
-
-        slider.preview.removeClass('invisible');  
+        }  
     } 
     else if(slider.animate){ // in case supporting animation
-      /* 
-        나열할 썸네일을 그린다. 
-        total개수를 모두 그릴 것인데 어디서부터 그리는가가 중요하다. 
-        가령, 첫 시작 위치가 5라면 5,6,7이 가장 첫 위치가 될 것이다.
-        혹은 1번부터 순차적으로 그린 후에 위치를 조정할 수 있을 것이다.
-        어떤 것이 덜 비용을 들이면서 구현할 수 있는지 확인을 해보자.
-        
-        이곳에서 움직임을 제어할 수 있도록 설정이 가능한가?
-        
-      */
-      
         // 일단은 0부터 시작해서 나열한 상태로 한다.
-        // 초기 설정에 맞게 해당 위치를 설정하여 가장 첫번째 위치하도록 한다.
+        // 초기 설정에 맞게 해당 위치를 설정하여 가장 첫번째 위치하도록 한다. Need to modify something on the loop
         if(slider.preview_setting.length === 0){
-            for(var el=1;el<=slider.total;el++){
+            for(el=1;el<=slider.total;el++){
                 slider.preview_setting.push(el);
             }
         }
 
-        var set = slider.preview_setting;
+        set = slider.preview_setting;
         for(i=0;i<set.length;i++){
-            str += '<a href="#none" class="img"><img src="' + slider.imgPath + set[i] + "." + 
-                slider.imgType + '" alt="' + set[i] + '" width="57" height="60" /></a>';
-        }
-        
-      
-        // 순서대로 입력한 후에 위치를 조정한다.
-        // DOM에 삽입하기 전에 위치를 조정한다.
-        // preview-thumbnail의 넓이를 세팅한다.
-        // 안에 있는 a태그를 세팅한다.
-        // 위치를 세팅하기 위해서는 각각의 이미지 썸네일을 탐색하려면 돔에 집어 넣어야 한다.
-        // 이 때문에 미리 넣은 다음 보여줄 수 있도록 해야 한다.
-        // invisible을 넣어주었다가 모든 처리가 완료되면 invisible을 제거한다.
-        
-        slider.preview.addClass('invisible');
-        slider.preview.html(str);
-        
-        // 넣은 후에 탐색할 것.
-        for(var ele=0;ele<slider.total;ele++){
-            $('.img').eq(ele).css({
-                'left' : 72*ele + 'px'
+            elem = '<a href="#none" class="img"><img src="' + slider.imgPath + set[i] + "." + 
+                slider.imgType + '" alt="' + set[i] + '" width="57" height="60" /></a>';;
+            slider.preview.append(elem).find('.img').eq(i).css({
+                'left' : 72*i + 'px'
             });
         }
-      
-        // 첫 위치를 조정할 것. direction을 받아서 해당 방향으로 한칸씩 이동한다.
-        // 보여주기 전에 위치를 세팅할 것.
-        // slider.preview.css({
-        //     'left' : '-' + 72*slider.current + 'px'
-        // });
-
-        // 모든 처리가 완료될 경우 보여줄 것.
-        slider.preview.removeClass('invisible');
     }
 }
 
-
-// @ current와 함께 위치를 찍어주자.??
+/*
+    
+*/
 function setPointerOnPreview(slider, direction, callback){
     
     slider.preview.find("img").removeClass("selected");
@@ -293,8 +251,6 @@ function setPointerOnPreview(slider, direction, callback){
         if(direction === 'next' && slider.current > 2){
             // 세번째 썸네일에 포인터가 있고 다음 버튼을 누를 경우 오른쪽으로 이동
             if((Math.abs(slider.preview_pos / 72)+3) === slider.current){
-                // console.log('do animate right direction');
-                
                 slider.preview_pos += 72;
                 slider.preview.stop().animate({
                     'left' : '-' + slider.preview_pos + 'px'
@@ -313,9 +269,11 @@ function setPointerOnPreview(slider, direction, callback){
     }
 }
 
-  
+/** 
+ * 
+ */
 function controlButton(slider){
-    if(!slider.loop || slider.loop === null){
+    if(!slider.loop || slider.loop === null){ // loop false
         
         if(slider.next !== null){
             slider.btnNext.removeClass("invisible");
@@ -329,9 +287,7 @@ function controlButton(slider){
             slider.btnPrev.addClass("invisible");
         }
         
-    }else{ // loop true
-        
-    }
+    }else{} // loop true
 }
 
 /*
@@ -363,6 +319,8 @@ function initialize(slider){ // 이 부분을 prototype에 등록할 것.
     7. turn view after initialization or loading
     8. in clicking the thumbnail in one of three.
     9. delegate 'controlButton' to some function and make it work after....
+    10. how to check elapse time each improvement
+    11. let know thumbnail's width automatically or insert option or so such as 72 or 73 pixels
 */
 var slider = new JSlider({
 	total : 8,
